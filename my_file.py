5370cced1b686203830819f239a450d64ac31a72
Balpreet kaur <balpreetkaur.it24@jecrc.ac.in>
2023-04-25 18:48:31 +0530

    file7

---
+++ my_file.py
@@ -0,0 +1,16 @@
+
+c=40
+
+
+def abc():
+     print('good message')
+
+#dunder    
+
+if __name__=='__main__':
+    print('oh ye to mail se call hua hai')
+    a=int(input('enter the first value'))
+    b=int(input('enter the second value'))
+    print(c*(a+b))
+    
+#the dunder part will executte only when this myfile.py runs not when myfile is imported in another file    
